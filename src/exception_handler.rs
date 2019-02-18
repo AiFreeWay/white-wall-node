80432765eb9c943cf0364c65a570a8505b7ae340
vladislav.a <vladyaaifree@gmail.com>
2019-02-18 08:09:13 +0300

    Work on KeyStorage

--- src/exception_handler.rs
+++ src/exception_handler.rs
@@ -4,15 +4,25 @@ pub struct Error {
 }
 
 pub enum ErrorTypes {
-    GenerateKeypair = 1
+    GenerateKeypair    = 1,
+    SaveKeySeed = 2,
 }
 
 impl Error {
     
-    pub fn keypair_error() -> Error {
+    pub fn generate_keypair_error(description: String) -> Error {
+        let description = format!("Error keypair generation: {}", description);
         return Error {
             id: ErrorTypes::GenerateKeypair as u8,
-            description: String::from("Error keypair generation")
+            description: description
+        }
+    }
+    
+    pub fn save_keyseed_error(description: String) -> Error {
+        let description = format!("Error keyseed saving: {}", description);
+        return Error {
+            id: ErrorTypes::SaveKeySeed as u8,
+            description: description
         }
     }
 }
--- src/storage/key_storage.rs
+++ src/storage/key_storage.rs
@@ -1,22 +1,25 @@
 use configuration;
 use ed25519_dalek::Keypair;
 use exception_handler::Error;
+use std::fs::File;
 
 
 struct Key {
     key: String,
-    creation_date: u32,
-    date_of_change: u32,
 }
 
 impl Key {
     
-    pub fn from_path(path: String) -> Key {
-        return Key {
-            key: String::from(""),
-            creation_date: 0,
-            date_of_change: 0
-        }
+    pub fn new(path: String, keypair: Keypair) {
+        save_key_file(path, keypair)
+    }
+    
+    pub fn from_path(path: String) -> Result<Key, Error> {
+        let key = Key {
+            key: read_key_file(path),
+        };
+        
+        return Ok(key);
     }
     
     pub fn get_key(&self) -> &str {
@@ -32,13 +35,23 @@ struct KeyStorage {
     key: Key
 }
 
-impl  KeyStorage {
+impl KeyStorage {
     
-    pub fn new() -> KeyStorage {
+    pub fn create(keyseed: String) -> Result<KeyStorage, Error> {
         let key_path = configuration::get_config().get_key_path();
         
-        return KeyStorage {
-            key: Key::from_path(key_path)
+        return match Key::new(key_path, keyseed) {
+            Ok(key) => KeyStorage { key: key },
+            Err(err) => err,
+        }
+    }
+    
+    pub fn open() -> Result<KeyStorage, Error> {
+        let key_path = configuration::get_config().get_key_path();
+        
+        return match Key::from_path(key_path) {
+            Ok(key) => KeyStorage { key: key },
+            Err(err) => err,
         }
     }
     
@@ -46,11 +59,22 @@ impl  KeyStorage {
         let generate_result = Keypair::from_bytes(self.key.get_key_bytes());
         return match generate_result {
             Ok(keypair) => Ok(keypair),
-            Err(e) => Err(Error::keypair_error())
+            Err(err) => Err(Error::generate_keypair_error(err.to_string()))
         }
     }
     
-    pub fn is_was_changed(&self) -> bool {
-        return true
+    fn read_key_file(path: str) -> Result<String, Error> {
+        match File::open(path) {
+            Ok(file) => {},
+            Err(error)
+        }
+    }
+    
+    fn save_key_file(path: String, keyseed: String) -> Result<String, Error> {
+        return std::fs::remove_file(path)
+            .map_err(|err| -> Error::save_keyseed_error(err.to_string()))
+            .and(|_| -> File::create(keyseed))
+            .and(|file| -> file::write_fmt(!format(path)))
+            .and(|_| -> path)
     }
 }
